d99e0b993fc9aec3c6af3c06b854dec00ebca992
PoetaKodu <rfpawelsyska@gmail.com>
2022-09-25 02:38:45 +0200

    fix: missing exports from `src/components` folder

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,7 @@
 export { default as Quiz }				from "./Quiz";
 export { default as Option }			from "./Option";
 export { default as StageNavigation }	from "./StageNavigation";
+export { default as StageTitle }		from "./StageTitle";
+export { default as StageOptions }		from "./StageOptions";
+export { default as StageLayout }		from "./StageLayout";
+export { default as Results }			from "./Results";
